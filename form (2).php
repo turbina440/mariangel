c5e157191193cf9fa0fd34018090d15479ce9e79
Diego Canteros <123302060+turbina440@users.noreply.github.com>
2023-03-05 10:57:48 -0300

    Update form (2).php

--- form (2).php
+++ form (2).php
@@ -4,7 +4,7 @@
 	<link rel="stylesheet" type="text/css" href="normalize.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="estilo (2).css">
+	<link rel="stylesheet" type="text/css" href="estilo (3).css">
 	<title>form</title>
 </head>
 <body>
